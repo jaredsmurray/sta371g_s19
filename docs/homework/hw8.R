6c8072362f335a4e05d399c945d5b16bdc0f33e5
Jared <jmurray.1022@gmail.com>
2019-04-25 23:11:57 -0500

    hw8

---
+++ docs/homework/hw8.R
@@ -0,0 +1,7 @@
+library(readr)
+# Load data for problem 1
+gas = read_csv("http://jgscott.github.io/teaching/data/GasPrices.csv")
+
+# Load data for problem 2
+cheese = read_csv(paste0('https://jgscott.github.io/STA371H_Spring2018/data/cheese.csv', 'orion.csv'))
+
